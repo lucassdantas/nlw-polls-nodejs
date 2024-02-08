73c6ee933bb22af71fa0ef4645873f957489a64b
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-08 20:46:46 -0300

    add prisma and create db

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,9 @@ import fastify from 'fastify'
 
 const app = fastify()
 
+app.get('/hello', (req:any, res:any) => {
+    res.send('<h1>oi</h1>')
+})
 app.listen({port:3333}).then(() => {
     console.log('running')
 })
