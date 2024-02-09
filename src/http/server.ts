8676616f7f60b41ca875ae6603465a5505b9602e
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-08 21:11:13 -0300

    refactoring folders

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,27 +1,11 @@
 import fastify from 'fastify'
-import {PrismaClient} from '@prisma/client'
-
-import {z} from 'zod'
+import { prisma } from '../lib/prisma'
+import { createPoll } from './routes/crate-poll'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
+app.register(createPoll)
 
-app.post('/polls', async (req, res) => {
-    const createPollBody = z.object({
-        title:z.string()
-    })
-
-    const {title} =  createPollBody.parse(req.body)
-
-    const poll = await prisma.poll.create({
-        data:{
-            title,
-        }
-    })
-
-    return res.status(201).send({pollId:poll.id})
-})
 
 const port = 3333
 app.listen({port:port}).then(() => {
