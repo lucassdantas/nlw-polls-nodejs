0f89484abea2e72959153633b6664ae824648296
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-09 00:25:55 -0300

    testing websocket

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -4,6 +4,11 @@ export async function pollResults(app: FastifyInstance){
     app.get('/polls/:pollId/results', {websocket:true}, (connection, request) => {
         connection.socket.on('message', (message:string) => {
             connection.socket.send('Você enviou: ' + message)
+
+            setTimeout(() => {
+                connection.socket.send('oi')
+            }, 2000)
+            
         })
     })
 }
