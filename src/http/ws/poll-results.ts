8465e515a5f56c89e0831a4c068a17f3522d8f7b
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-09 00:27:57 -0300

    preapring to new code

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -3,11 +3,6 @@ import { FastifyInstance } from "fastify";
 export async function pollResults(app: FastifyInstance){
     app.get('/polls/:pollId/results', {websocket:true}, (connection, request) => {
         connection.socket.on('message', (message:string) => {
-            connection.socket.send('Você enviou: ' + message)
-
-            setTimeout(() => {
-                connection.socket.send('oi')
-            }, 2000)
             
         })
     })
